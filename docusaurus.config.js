e6601ec980b08d5321934e8512a54c9a35b31cb8
su-mao-xiong <sumaoxiong3963@gmail.com>
2026-04-20 21:53:36 +0800

    fix: baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://sumaoxiong.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/personal-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
