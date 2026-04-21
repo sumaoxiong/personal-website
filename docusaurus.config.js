e4919f5dba25cdbe3a9f3d9c437717b48227990b
su-mao-xiong <sumaoxiong3963@gmail.com>
2026-04-22 00:09:07 +0800

    網站預設模式

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,6 +79,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
+        defaultMode: 'light',
         respectPrefersColorScheme: true,
       },
       navbar: {
